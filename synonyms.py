939f43a77dc0af19776ca46ba06c8e1bb31026cd
Milan J S <milan.j.srinivas@gmail.com>
2019-06-02 15:57:38 +0000

    Uncomment code

--- synonyms.py
+++ synonyms.py
@@ -11,26 +11,26 @@ f.close()
 for item in tenwords:
     words.append(item.strip().lower())
 
-#model = word2vec.Word2Vec(sentences, size=50, window=5, min_count=1, workers=4,hs=1,negative=0)
+model = word2vec.Word2Vec(sentences, size=50, window=5, min_count=1, workers=4,hs=1,negative=0)
 
-# tensor = []
-# for word in words:
-#     tensor.append(model.wv[word])
+tensor = []
+for word in words:
+    tensor.append(model.wv[word])
 
-# nbrs = NearestNeighbors(n_neighbors=3, algorithm='ball_tree').fit(tensor)
-# distances, indices = nbrs.kneighbors([model.wv['juice']])
+nbrs = NearestNeighbors(n_neighbors=3, algorithm='ball_tree').fit(tensor)
+distances, indices = nbrs.kneighbors([model.wv['juice']])
 
-# for index in indices[0]:
-#     print(words[index])
+for index in indices[0]:
+    print(words[index])
 
- # pickle the entire model to disk, so we can load&resume training later
-#model.save('text8.model')
-# store the learned weights, in a format the original C tool understands
-#model.wv.save_word2vec_format('text8.model.bin', binary=True)
+ pickle the entire model to disk, so we can load&resume training later
+model.save('text8.model')
+store the learned weights, in a format the original C tool understands
+model.wv.save_word2vec_format('text8.model.bin', binary=True)
 
-#model = word2vec.Word2Vec.load('text8.model')
+model = word2vec.Word2Vec.load('text8.model')
 
-#model.train([words], epochs=100 , total_words=len(words))
+model.train([words], epochs=100 , total_words=len(words))
 
 #model.save('text8.model')
 #model.wv.save_word2vec_format('text8.model.bin', binary=True)
